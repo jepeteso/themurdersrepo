80eafed982b8858ec36c11ae4de98ec9a7a2a523
jepeteso <jerssonplacido@gmail.com>
2020-12-01 18:09:22 -0500

    first commit, adding wrangle-data.R fill just to get started

---
+++ wrangle-data.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+murders <- read_csv("data/murders.csv")
+murders <- murders%>% mutate(region =factor(region), rate= total/population*10^5)
+save(murders, file ="rda/murders.rda")
+
+library(tidyverse)
+murders <- read_csv("data/murders.csv")
+murders <- murders%>% mutate(region =factor(region), rate= total/population*10^5)
+save(murders, file ="abc/murders.rda")
